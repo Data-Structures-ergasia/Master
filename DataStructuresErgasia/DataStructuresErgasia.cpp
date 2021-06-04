9809e8b42cf2476f3007595a899a9137059e544e
lamchatz <63646426+lamchatz@users.noreply.github.com>
2021-06-04 20:37:47 +0300

    Add files via upload

---
+++ DataStructuresErgasia/DataStructuresErgasia.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <sstream>
+using namespace std;
+
+string removeAnnotation(string str)
+{
+    string word = "";
+    for (char c : str)
+    {
+        if (isalpha(c))
+        {
+            word = word + c;
+        }
+        else if (c == ' ')
+        {
+            word = word + c;
+        }
+    }
+    return word;
+}
+
+int main()
+{
+    string word, line;
+    ifstream myfile;
+    myfile.open("small-file.txt");
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            if (!line.empty())
+            {
+                line = removeAnnotation(line);
+                stringstream stringstream(line);
+                while (getline(stringstream, word, ' '))
+                {
+                    cout << word << "\n";
+                }
+            }
+                }
+
+        myfile.close();
+    }
+    else
+    {
+        cout << ":;;;";
+    }
+
+    return 0;
+}
+
+/*while(stringstream.good() && i<10){
+         stringstream >> arr[i];
+         i++;
+       }
+       for(i = 0; i < 10; i++){
+        cout << arr[i] << endl;
+    } */
